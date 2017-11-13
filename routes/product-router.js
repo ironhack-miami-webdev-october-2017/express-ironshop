4d57540620ee93e1353d624e98566138758f722e
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-11-13 16:52:06 -0500

    Add creation of products

--- routes/product-router.js
+++ routes/product-router.js
@@ -21,9 +21,42 @@ router.get("/products", (req, res, next) => {
           res.render("product-views/product-list");
       })
       .catch((err) => {
-          // to be determined...
+          // render the error page with our error
+          next(err);
       });
 }); // GET /products
+
+
+// STEP #1: show the new product form
+router.get("/products/new", (req, res, next) => {
+    res.render("product-views/product-form");
+}); // GET /products/new
+
+// STEP #2: process the new product submission
+router.post("/products", (req, res, next) => {
+    const theProduct = new ProductModel({
+        name:        req.body.productName,
+        price:       req.body.productPrice,
+        imageUrl:    req.body.productImage,
+        description: req.body.productDescription,
+        dateAdded:   new Date()  // |
+    }); // |                     // |
+        // fields from         names of the
+        // model's schema      input tags
+
+    theProduct.save()
+      .then(() => {
+          // STEP #3: redirect after a SUCCESSFUL save
+          // redirect to the list of products page
+          res.redirect("/products");
+            // you can't redirect to an EJS file
+            // you can ONLY redirect to a URL
+      })
+      .catch((err) => {
+          // render the error page with our error
+          next(err);
+      });
+}); // POST /products
 
 
 module.exports = router;
